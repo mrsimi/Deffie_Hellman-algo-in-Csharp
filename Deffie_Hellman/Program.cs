e459517c761d9dd703271e9b990b199d37325334
Simi <adegokesimi@gmail.com>
2018-10-28 01:29:52 +0100

    Initial Commit

---
+++ Deffie_Hellman/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Deffie_Hellman
+{
+    class Alice
+    {
+        public static byte[] alicePublicKey;
+
+        public static void Main(string[] args)
+        {
+            using (ECDiffieHellmanCng alice = new ECDiffieHellmanCng())
+            {
+                string fileName = @"C:\Users\Simi\Desktop\notes\novel.txt";
+                string outputfile = @"C:\Users\Simi\Desktop\notes\output1.txt";
+
+                StreamReader reader = new StreamReader(fileName);
+                string message = reader.ReadToEnd();
+                StreamWriter writer = new StreamWriter(outputfile);
+
+                alice.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                alice.HashAlgorithm = CngAlgorithm.Sha256;
+                alicePublicKey = alice.PublicKey.ToByteArray();
+
+                Bob bob = new Bob();
+                byte[] aliceKey = alice.DeriveKeyMaterial(CngKey.Import(bob.bobPublicKey, CngKeyBlobFormat.EccPublicBlob));
+                byte[] encryptedMessage = null;
+                byte[] iv = null;
+
+                Console.WriteLine("Secret Message");
+                Send(aliceKey, message, out encryptedMessage, out iv);
+                writer.Write(Encoding.UTF8.GetString(encryptedMessage));
+                writer.Close();
+
+                Console.WriteLine("Decrypted Message");
+                bob.Recieve(encryptedMessage, iv);
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void Send(byte[] key, string secretMessage, out byte[] encryptedMessage, out byte[] iv)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                iv = aes.IV;
+
+                //encrypt the message
+                using (MemoryStream ciphertext = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ciphertext,
+                    aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plaintextMessage = Encoding.UTF8.GetBytes(secretMessage);
+                    cs.Write(plaintextMessage, 0, plaintextMessage.Length);
+                    cs.Close();
+                    encryptedMessage = ciphertext.ToArray();
+                }
+            }
+        }
+    }
+
+    public class Bob
+    {
+        public byte[] bobPublicKey;
+        private byte[] bobKey;
+
+        public Bob()
+        {
+            using (ECDiffieHellmanCng bob = new ECDiffieHellmanCng())
+            {
+                bob.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                bob.HashAlgorithm = CngAlgorithm.Sha256;
+                bobPublicKey = bob.PublicKey.ToByteArray();
+                bobKey = bob.DeriveKeyMaterial(CngKey.Import(Alice.alicePublicKey, CngKeyBlobFormat.EccPublicBlob));
+            }
+        }
+
+        public void Recieve(byte[] encryptedMessage, byte[] iv)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = bobKey;
+                aes.IV = iv;
+                //Decrypt the message
+                using (MemoryStream plaintext = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(plaintext,
+                        aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedMessage, 0, encryptedMessage.Length);
+                        cs.Close();
+                        string message = Encoding.UTF8.GetString(plaintext.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
